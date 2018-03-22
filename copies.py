648a2ffadc53b2371355428d972604487dba30f4
caravloan <32499436+caravloan@users.noreply.github.com>
2018-03-22 17:32:39 -0500

    Create copies.py

---
+++ copies.py
@@ -0,0 +1,21 @@
+def go_bananas(x):
+  return x + "is bananas!"
+go_bananas("coding")
+
+def speak_spanish (m):
+  m[0] = "hola"
+  return
+  
+test = ["cara", "iqra"]
+print test
+speak_spanish (test)
+print test
+
+listy_list = ["Fruit", "Gummy", "Water", "Pizza", "Hamburger"]
+listy_list.append("Mittens")
+print listy_list
+listy_list.pop(4)
+print listy_list
+
+for item in listy_list :
+  print item
